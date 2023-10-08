daf54604df759d2e4fbf0e1a9150fac19da8e2c9
Khalil-Burns <khalilburns@gmail.com>
2023-10-07 21:02:15 -0400

    updates

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -5,6 +5,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Config {
@@ -35,6 +43,7 @@ public final class Config {
    */
   private static int robotId = -1;
 
+  
   /**
    * Returns one of the values passed based on the robot ID
    *
@@ -89,9 +98,200 @@ public final class Config {
    * 
    * 
    **/
+  
+  /**
+     * CAN IDs, ports, channels, etc.
+     */
+    public static class CANID {   
+      public static int PIGEON = robotSpecific(30, 27, 27, 27, 30);
+  
+      public static int CANDLE = robotSpecific(15, 15, -1, 15, 15);
+      public static int CTRE_PCM = robotSpecific(1, 1, -1, -1);
+
+      // Swerve Drive
+      public static final int FRONT_LEFT_DRIVE = 24;
+      public static final int REAR_LEFT_DRIVE = 20;
+      public static final int FRONT_RIGHT_DRIVE = 21;
+      public static final int REAR_RIGHT_DRIVE = 27;
+
+      public static final int FRONT_LEFT_STEERING = 23;
+      public static final int REAR_LEFT_STEERING = 26;
+      public static final int FRONT_RIGHT_STEERING = 25;
+      public static final int REAR_RIGHT_STEERING = 22;
+
+      public static final int FRONT_LEFT_CANCODER = 9;
+      public static final int REAR_LEFT_CANCODER = 8;
+      public static final int FRONT_RIGHT_CANCODER = 6;
+      public static final int REAR_RIGHT_CANCODER = 7;
+
+      // Arm Subsystem
+      public static final int TOP_ARM_SPARK_CAN_ID = robotSpecific(5, 0, 0, 0, 0, 18, 18);
+      public static final int BOTTOM_ARM_SPARK_CAN_ID = robotSpecific(4, 0, 0, 0, 0, 19, 19);
+      public static final int TOP_CANCODER_CAN_ID = 3;
+      public static final int BOTTOM_CANCODER_CAN_ID = 2;
+
+      // Differential Drive CAN IDs
+      public static int DIFF_LEADER_LEFT = robotSpecific(-01, 6, 2, 5, -01, 35);
+      public static int DIFF_LEADER_RIGHT = robotSpecific(-01, 3, 1, 3, -01, 33);
+      public static int DIFF_FOLLOWER_LEFT = robotSpecific(-01, 5, -1, 7, -01, 37);
+      public static int DIFF_FOLLOWER_RIGHT = robotSpecific(-01, 2, -1, 9, -01, 39);
+  
+      // Clutch CAN IDs
+      public static int INTAKE = robotSpecific(-01, 8, -1, -1);
+      public static int SHOOTER = robotSpecific(-01, 11, 5, -1);
+      public static int CLIMBER = robotSpecific(-01, 4, -1, -1);
+      public static int INDEXER = robotSpecific(-01, 7, 7, -1);
+  }
+  public static class Swerve{
+    public static final int steeringCurrentLimit = 20;
+    public static final int driveCurrentLimit = 50;
+
+    public static final double driveVoltComp = 12.0;
+    public static final double steeringVoltComp = 12.0;
+
+    public static final double kPXController = 1;
+    public static final double kPYController = 1;
+    public static final double kPThetaController = 1.35;
+
+    public static final double FL_ENCODER_OFFSET = 90.73;
+    public static final double FR_ENCODER_OFFSET = 204.3;
+    public static final double RL_ENCODER_OFFSET = 168.9;
+    public static final double RR_ENCODER_OFFSET = -5.5; 
+
+    public static boolean INVERTED_FRONT_LEFT_DRIVE = robotSpecific(false);
+    public static boolean INVERTED_REAR_LEFT_DRIVE =  robotSpecific(false);
+    public static boolean INVERTED_FRONT_RIGHT_DRIVE = robotSpecific(false);
+    public static boolean INVERTED_REAR_RIGHT_DRIVE = robotSpecific(false);
+
+    public static boolean INVERTED_FRONT_LEFT_STEERING =  robotSpecific(false);
+    public static boolean INVERTED_REAR_LEFT_STEERING =  robotSpecific(false);
+    public static boolean INVERTED_FRONT_RIGHT_STEERING = robotSpecific(false);
+    public static boolean INVERTED_REAR_RIGHT_STEERING =  robotSpecific(false);
+
+    public static double ENCODER_SYNCING_PERIOD = 0.5; // seconds
+    public static int ENCODER_SYNCING_TIMEOUT = 40; // seconds
+
+    public static double TIME_FOR_BRAKES_IN_AUTO = 14.9; // seconds
+
+    public static final double MK4_L1_GEAR_RATIO = (50.0/14.0)*(19.0/25.0)*(45.0/15.0);
+    public static final double turningEncoderConstant = (2*Math.PI)/12.8;
+    public static final double drivePositionConversionFactor = drivetrainWheelDiameter * Math.PI / MK4_L1_GEAR_RATIO;
+    public static final double driveVelocityConversionFactor = drivePositionConversionFactor / 60.0;
+
+    public static final IdleMode defaultDriveIdleMode = IdleMode.kBrake;
+    public static final IdleMode defaultSteeringIdleMode = IdleMode.kCoast;
+
+    public static final double drive_kIZone = 0.0;
+    public static final double drive_kFF = 0.0; // These can also be module specific.
+    public static final double drive_kP = 0.2; // Hopefully they won't need to be.
+    public static final double drive_kI = 0.0; // Depends on hardware differences.
+    public static final double drive_kD = 0.0;
+    
+    public static DoubleSubscriber sub_drive_kFF = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Drive kFF").subscribe(drive_kFF);
+    public static DoubleSubscriber sub_drive_kP = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Drive kP").subscribe(drive_kP);
+    public static DoubleSubscriber sub_drive_kI = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Drive kI").subscribe(drive_kI);
+    public static DoubleSubscriber sub_drive_kD = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Drive kD").subscribe(drive_kD);
+    public static DoubleSubscriber sub_drive_kIZone = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Drive kIzone").subscribe(drive_kIZone);
 
 
+    public static final double steering_kFF = 0.0;
+    public static final double steering_kP = 1.0;
+    public static final double steering_kI = 0.0;
+    public static final double steering_kD = 0.1;
+    public static final double steering_kIZone = 0.0; //5 degrees
 
+    public static DoubleSubscriber sub_steering_kFF = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Steering kFF").subscribe(steering_kFF);
+    public static DoubleSubscriber sub_steering_kP = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Steering kP").subscribe(steering_kP);
+    public static DoubleSubscriber sub_steering_kI = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Steering kI").subscribe(steering_kI);
+    public static DoubleSubscriber sub_steering_kD = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Steering kD").subscribe(steering_kD);
+    public static DoubleSubscriber sub_steering_kIZone = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Steering kIzone").subscribe(steering_kIZone);
 
-   /** ADD CONSTANTS BELOW THIS LINE */
+    public static final SwerveDriveKinematics kSwerveDriveKinematics = new SwerveDriveKinematics(
+        new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(kWheelBase / 2, -kTrackWidth / 2),   
+        new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    // Max speeds for desaturating
+    public static final double kMaxAttainableWheelSpeed = 3.0; // Used by Auto, do not change.
+    
+    // ~ Teleop speeds. Speeds should be portional to kMaxAttainableAngularSpeed. Angular speeds should be specified.
+       
+    // Default Speeds
+    public static final double teleopDefaultSpeed = kMaxAttainableWheelSpeed;// Full speed
+    public static final double teleopDefaultAngularSpeed = Math.PI*3.0; // Old value: Math.PI*2.0
+
+    // Left Bumper speeds
+    public static final double teleopLeftBumperSpeed = kMaxAttainableWheelSpeed / 3.0;// A third of full speed
+    public static final double teleopLeftBumperAngularSpeed = Math.PI; // Can be a slower angular speed if wanted
+
+    // Right bumper speeds
+    public static final double teleopRightBumperSpeed = 0.3;
+    public static final double teleopRightBumperAngularSpeed = 0.3;
+    
+
+    // Old PathWeaver speeds
+    public static final double kMaxAutoSpeed = 3; // m/s
+    public static final double kMaxAutoAcceleration = 3; // m/s/s
+    public static final double kMaxAutoAngularSpeed = Math.PI *3; // rad/s
+    public static final double kMaxAutoAngularAcceleration = Math.PI * 3; // rad/s/s
+
+    // Acceleration limit for teleop
+    public static final double teleopRateLimit = 3;
+
+    public static final double driveKS = 0.667;
+    public static final double driveKV = 2.9;
+    public static final double driveKA = 0.5;
+
+    public static DoubleSubscriber sub_kA = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Drive kA").subscribe(driveKA);
+    public static DoubleSubscriber sub_kV = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Drive kV").subscribe(driveKV);
+    public static DoubleSubscriber sub_kS = NetworkTableInstance.getDefault().getTable("SwerveChassis/DrivePID").getDoubleTopic("Drive kS").subscribe(driveKS);
+
+    
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+        kMaxAutoAngularSpeed, kMaxAutoAngularAcceleration);
 }
+
+
+
+
+  /** ADD CONSTANTS BELOW THIS LINE */
+
+
+  public static Double DRIVER_JOYSTICK_DEADBAND = 0.1; // TODO: Investigate if this can be better tuned
+  
+  /** Drivetrain Constants */
+  public static final double kTrackWidth = robotSpecific(0.655, 1.2267, 0.3136, 0.569, 0.52, 0.51762);
+  public static final double kWheelBase = robotSpecific(0.52,-0.0,-0.0,-0.0,-0.0,-0.0);
+  public static double drivetrainWheelDiameter = robotSpecific(0.0986536, 0.1524, 0.1016, 0.1524, 0.01524);
+
+  public static DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackWidth);
+  
+  /* Swerve Voltage Compensation */
+    public static final double voltageComp = 12.0;
+
+  /* Swerve Current Limiting */
+  public static final int angleContinuousCurrentLimit = 20;
+  public static final int driveContinuousCurrentLimit = 80;
+
+  /* Angle Motor PID Values */
+  //FIND OUT: if any of below is bot specific
+  public static final double angleKP = 0.01;
+  public static final double angleKI = 0.0;
+  public static final double angleKD = 0.0;
+  public static final double angleKFF = 0.0;
+
+  /* Drive Motor PID Values */
+  public static final double driveKP = 0.1;
+  public static final double driveKI = 0.0;
+  public static final double driveKD = 0.0;
+  public static final double driveKFF = 0.0;
+
+  /* Drive Motor Characterization Values */
+  public static final double driveKS = 0.667;
+  public static final double driveKV = 2.44;
+  public static final double driveKA = 0.27;
+  
+
+}
+
---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,325 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.ctre.phoenix.sensors.SensorTimeBase;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.lib.lib2706.math.OnboardModuleState;
+import frc.lib3512.util.CANCoderUtil;
+import frc.lib3512.util.CANSparkMaxUtil;
+import frc.lib3512.util.CANCoderUtil.CCUsage;
+import frc.lib3512.util.CANSparkMaxUtil.Usage;
+import frc.robot.config.Config;
+import static frc.robot.ErrorCheck.errCTRE;
+import static frc.robot.ErrorCheck.errREV;
+
+
+public class SwerveModule {
+
+    // CODE: Prepare 2 variables for both SparkMaxs, use the object called CANSparkMax
+    private CANSparkMax m_driveMotor;
+    private CANSparkMax m_turningMotor;
+    private RelativeEncoder m_driveEncoder;
+    private RelativeEncoder m_turningEncoder;
+    private SparkMaxPIDController m_drivePIDController;
+    private SparkMaxPIDController m_turningPIDController;
+    private CANCoder encoder;
+    private double lastAngle;
+    private NetworkTable swerveModuleTable;
+    private DoublePublisher desiredSpeedEntry;
+    private DoublePublisher desiredAngleEntry;
+    private DoublePublisher currentSpeedEntry;
+    private DoublePublisher currentAngleEntry;
+    private DoublePublisher speedError;
+    private DoublePublisher angleError;
+    private DoublePublisher desiredAngle360Range;
+    private DoublePublisher currentAngle360Range;
+    private double m_encoderOffset;
+    private DoublePublisher canCoderEntry;
+    private DoubleSubscriber sub_offset;
+
+
+    SimpleMotorFeedforward feedforward = 
+        new SimpleMotorFeedforward(Config.Swerve.driveKS, Config.Swerve.driveKV, Config.Swerve.driveKA);
+    /**
+     * Constructs a SwerveModule.
+     */
+    public SwerveModule(int driveCanID, boolean driveInverted, int turningCanID, boolean turningInverted, int encoderCanID, double encoderOffset, String ModuleName) {
+
+        // CODE: Construct both CANSparkMax objects and set all the nessecary settings (CONSTANTS from Config or from the parameters of the constructor)
+        m_driveMotor = new CANSparkMax(driveCanID, MotorType.kBrushless);
+
+        errREV(m_driveMotor.restoreFactoryDefaults());
+        m_driveMotor.setInverted(driveInverted);
+        errREV(m_driveMotor.setIdleMode(Config.Swerve.defaultDriveIdleMode));
+        errREV(m_driveMotor.setSmartCurrentLimit(Config.Swerve.driveCurrentLimit));
+        errREV(m_driveMotor.enableVoltageCompensation(Config.Swerve.driveVoltComp));
+
+        m_drivePIDController = m_driveMotor.getPIDController();
+        errREV(m_drivePIDController.setP(Config.Swerve.sub_drive_kP.get()));
+        errREV(m_drivePIDController.setI(Config.Swerve.sub_drive_kI.get()));
+        errREV(m_drivePIDController.setD(Config.Swerve.sub_drive_kD.get()));
+        errREV(m_drivePIDController.setIZone(Config.Swerve.sub_drive_kIZone.get()));
+        errREV(m_drivePIDController.setFF(Config.Swerve.sub_drive_kFF.get()));   
+        CANSparkMaxUtil.setCANSparkMaxBusUsage(m_driveMotor, Usage.kAll);
+        
+        m_turningMotor = new CANSparkMax(turningCanID, MotorType.kBrushless);
+        errREV(m_turningMotor.restoreFactoryDefaults());
+        m_turningPIDController = m_turningMotor.getPIDController();
+        errREV(m_turningMotor.enableVoltageCompensation(Config.Swerve.steeringVoltComp));
+        errREV(m_turningMotor.setSmartCurrentLimit(Config.Swerve.steeringCurrentLimit));
+
+        m_driveEncoder = m_driveMotor.getEncoder();
+        errREV(m_driveEncoder.setPositionConversionFactor(Config.Swerve.drivePositionConversionFactor));
+        errREV(m_driveEncoder.setVelocityConversionFactor(Config.Swerve.driveVelocityConversionFactor));
+        errREV(m_driveEncoder.setPosition(0.0));
+
+        
+        CANSparkMaxUtil.setCANSparkMaxBusUsage(m_turningMotor, Usage.kPositionOnly);
+        m_turningMotor.setInverted(turningInverted);
+        errREV(m_turningMotor.setIdleMode(Config.Swerve.defaultSteeringIdleMode));
+
+        m_turningEncoder = m_turningMotor.getEncoder();
+        errREV(m_turningEncoder.setPositionConversionFactor(Config.Swerve.turningEncoderConstant));
+
+        errREV(m_turningPIDController.setP(Config.Swerve.sub_steering_kP.get()));
+        errREV(m_turningPIDController.setI(Config.Swerve.sub_steering_kI.get()));
+        errREV(m_turningPIDController.setD(Config.Swerve.sub_steering_kD.get()));
+        errREV(m_turningPIDController.setIZone(Config.Swerve.sub_steering_kIZone.get()));
+        errREV(m_turningPIDController.setFF(Config.Swerve.sub_steering_kFF.get()));
+
+        errREV(m_driveMotor.burnFlash());
+        errREV(m_turningMotor.burnFlash());
+
+        String tableName = "SwerveChassis/SwerveModule" + ModuleName;
+        swerveModuleTable = NetworkTableInstance.getDefault().getTable(tableName);
+
+        CANCoderConfiguration encoderConfiguration = new CANCoderConfiguration();
+        encoderConfiguration.initializationStrategy = 
+            SensorInitializationStrategy.BootToAbsolutePosition;
+        encoderConfiguration.sensorTimeBase = SensorTimeBase.PerSecond;
+        encoderConfiguration.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+        encoderConfiguration.magnetOffsetDegrees = 0;
+        encoderConfiguration.sensorDirection = direction == Direction.CLOCKWISE;
+
+        encoder = new CANCoder(encoderCanID);
+
+        errCTRE(encoder.configFactoryDefault());
+        CANCoderUtil.setCANCoderBusUsage(encoder, CCUsage.kMinimal);
+        errCTRE(encoder.configAllSettings(encoderConfiguration));
+        
+        desiredSpeedEntry = swerveModuleTable.getDoubleTopic("Desired speed (mps)").publish();
+        desiredAngleEntry = swerveModuleTable.getDoubleTopic("Desired angle (deg)").publish();
+        currentSpeedEntry = swerveModuleTable.getDoubleTopic("Current speed (mps)").publish();
+        currentAngleEntry = swerveModuleTable.getDoubleTopic("Current angle (deg)").publish();
+        speedError = swerveModuleTable.getDoubleTopic("Speed error (mps)").publish();
+        angleError = swerveModuleTable.getDoubleTopic("Angle error (deg)").publish(); 
+        canCoderEntry = swerveModuleTable.getDoubleTopic("CanCoder").publish();
+        desiredAngle360Range = swerveModuleTable.getDoubleTopic("Desired angle 360 range").publish();
+        currentAngle360Range = swerveModuleTable.getDoubleTopic("Current angle 360 range").publish();
+        sub_offset = swerveModuleTable.getDoubleTopic("offset").subscribe(encoderOffset);
+        
+        updateSteeringFromCanCoder();
+
+        resetLastAngle();
+    }
+
+    /**
+     * Returns the current state of the module.
+     *
+     * @return The current state of the module.
+     */
+    public SwerveModuleState getState() {   
+        return new SwerveModuleState(getVelocity(), getSteeringAngle());
+    }
+
+    /**
+     * Returns the current state of the module.
+     *
+     * @return The current state of the module.
+     */
+    public SwerveModulePosition getModulePosition() {   
+        return new SwerveModulePosition(getDrivePosition(), getSteeringAngle());
+    }
+
+    /**
+     * Sets the desired state for the module.
+     *
+     * @param desiredState Desired state with speed and angle.
+     */
+
+    public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop, boolean useAntiJitter) {
+        Rotation2d angle = getSteeringAngle();
+        double velocity = getVelocity();
+
+        desiredState = 
+            OnboardModuleState.optimize(
+                desiredState, angle);
+        
+
+        if (isOpenLoop){
+            double percentOutput = desiredState.speedMetersPerSecond/Config.Swerve.kMaxAttainableWheelSpeed;
+            m_driveMotor.set(percentOutput);
+        } else{
+            errREV(m_drivePIDController.setReference(desiredState.speedMetersPerSecond, ControlType.kVelocity, 0, feedforward.calculate(desiredState.speedMetersPerSecond)));
+        }
+
+        double newAngle;
+        if (useAntiJitter) {
+            if (Math.abs(desiredState.speedMetersPerSecond) <= Config.Swerve.kMaxAttainableWheelSpeed *0.001) {
+                newAngle = lastAngle;
+            } else {
+                newAngle = desiredState.angle.getRadians();
+            }
+        } else {
+            newAngle = desiredState.angle.getRadians();
+        }
+        
+        errREV(m_turningPIDController.setReference(newAngle, ControlType.kPosition));
+
+        lastAngle = newAngle;
+
+        desiredSpeedEntry.accept(desiredState.speedMetersPerSecond);
+        desiredAngleEntry.accept(desiredState.angle.getDegrees());
+        currentSpeedEntry.accept(velocity);
+        currentAngleEntry.accept(angle.getDegrees());
+        speedError.accept(desiredState.speedMetersPerSecond - velocity);
+        angleError.accept(desiredState.angle.getDegrees() - angle.getDegrees());
+        desiredAngle360Range.accept(MathUtil.inputModulus(desiredState.angle.getDegrees(), -180.0, 180.0));
+        currentAngle360Range.accept(MathUtil.inputModulus(angle.getDegrees(), -180.0, 180.0));
+        NetworkTableInstance.getDefault().flush();
+    }
+
+    public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {    
+        setDesiredState(desiredState, isOpenLoop, true);
+    }
+
+    public void resetLastAngle() {
+        lastAngle = getState().angle.getRadians();
+    }
+
+    /**
+     * Returns the velocity of the wheel in meters per second.
+     * 
+     * @return meters per second of the wheel
+     */
+    public double getDrivePosition() {
+
+        // CODE: Read encoder velocity from drive SparkMax and return m/s. (VelocityConversionFactor set so SparkMax returns m/s))
+        
+        return m_driveEncoder.getPosition();
+    }
+
+    /**
+     * Returns the velocity of the wheel in meters per second.
+     * 
+     * @return meters per second of the wheel
+     */
+    public double getVelocity() {
+
+        // CODE: Read encoder velocity from drive SparkMax and return m/s. (VelocityConversionFactor set so SparkMax returns m/s))
+        
+        return m_driveEncoder.getVelocity();
+    }
+
+    /**
+     * Returns the angle the wheel is pointing in a Rotation2d.
+     * 
+     * @return angle of the wheel as a Rotation2d
+     */
+    public Rotation2d getSteeringAngle() {
+
+        // CODE: Read encoder position from steering SparkMax and return Rotation2d.
+        // The PositionConversionFactor is set so SparkMax returns radians, the default constructor of Rotation2d wants radians.
+
+        return new Rotation2d(m_turningEncoder.getPosition());
+    }
+
+    /**
+     * Returns the Cancoder + offset.
+     * @return radians
+     */
+    public double getCancoder() {
+        return Math.toRadians(encoder.getAbsolutePosition() + sub_offset.get());
+    }
+
+    /**
+     * Gets a reading from the CanCoder and updates the SparkMax encoder (interal NEO encoder).
+     * This is specific to Swerge. Other methods need to be written for other hardware.
+     */
+    public void updateSteeringFromCanCoder() {
+        errREV(m_turningEncoder.setPosition(getCancoder()));
+
+    }
+
+    public void updatePIDValues(){
+        errREV(m_drivePIDController.setP(Config.Swerve.sub_drive_kP.get()));
+        errREV(m_drivePIDController.setI(Config.Swerve.sub_drive_kI.get()));
+        errREV(m_drivePIDController.setD(Config.Swerve.sub_drive_kD.get()));
+        errREV(m_drivePIDController.setIZone(Config.Swerve.sub_drive_kIZone.get()));
+        errREV(m_drivePIDController.setFF(Config.Swerve.sub_drive_kFF.get()));
+
+        errREV(m_turningPIDController.setP(Config.Swerve.sub_steering_kP.get()));
+        errREV(m_turningPIDController.setI(Config.Swerve.sub_steering_kI.get()));
+        errREV(m_turningPIDController.setD(Config.Swerve.sub_steering_kD.get()));
+        errREV(m_turningPIDController.setIZone(Config.Swerve.sub_steering_kIZone.get()));
+        errREV(m_turningPIDController.setFF(Config.Swerve.sub_steering_kFF.get()));
+
+        feedforward = new SimpleMotorFeedforward(Config.Swerve.sub_kS.get(), Config.Swerve.sub_kV.get(), Config.Swerve.sub_kA.get());
+    
+    }
+
+    public void updateNT(){
+        canCoderEntry.accept(Math.toDegrees(getCancoder()));//m_turningEncoder.getPosition()));
+    }
+
+    /**
+     * Standard stop motors method for every subsystem.
+     */
+    public void stopMotors() {
+        // CODE: Call the stopMotors method in the CANSparkMax (provided with all WPILib motor controller objects)
+        m_driveMotor.stopMotor();
+        m_turningMotor.stopMotor();
+    }
+
+    /**
+     * Checks if the Neo encoder is synced with the CanCoder
+     * NOTE: This only works before the first enable since optimizing messes it up
+     * 
+     * @return Whether the encoders are synced or not
+     */
+    public boolean areSteeringEncodersSynced() {
+        return Math.abs(getCancoder() - getSteeringAngle().getRadians()) < 0.0017;
+    }
+
+    private Direction direction = Direction.COUNTER_CLOCKWISE;
+    public enum Direction {
+        CLOCKWISE,
+        COUNTER_CLOCKWISE
+    }
+
+    public void setEncoderZero() {
+        m_turningEncoder.setPosition(0);
+    }
+}
