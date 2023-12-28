df2829bc48a0c074c4b6e8cc45f713537af5e52d
ErikCald <erikcaell@gmail.com>
2023-12-27 22:14:36 -0500

    Update venderdeps

--- src/main/java/frc/robot/commands/GrabGamePieceCommand.java
+++ src/main/java/frc/robot/commands/GrabGamePieceCommand.java
@@ -2,6 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class GrabGamePiece extends Command{
+public class GrabGamePieceCommand extends Command{
     
 }
